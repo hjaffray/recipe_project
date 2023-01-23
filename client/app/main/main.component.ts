ba7ad803af7dfbdf89d0c43c4068de5230cf449f
Henry Jaffray <henry.jaffray@du.edu>
2023-01-23 19:26:23 +0000

    Completed lab 10

--- client/app/main/main.component.ts
+++ client/app/main/main.component.ts
@@ -8,9 +8,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MainComponent implements OnInit {
 
+    public values: string[];
+    public input: string;
     static parameters = [HttpClient];
     constructor(private http: HttpClient) {
         this.http = http;
+        this.values = ['first', 'second', 'third'];
     }
 
     ngOnInit() {
