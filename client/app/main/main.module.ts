5077f2b43d07ec3b18c7ca5a65683e9e8e59cc05
Henry Jaffray <henry.jaffray@du.edu>
2023-01-24 17:08:58 +0000

    Completed Lab 11

--- client/app/main/main.module.ts
+++ client/app/main/main.module.ts
@@ -10,12 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MainComponent } from './main.component';
 
+import {UserService} from '../../components/services/user.service';
+import {SquarePipe} from '../../components/pipes/square.pipe';
 
 export const ROUTES: Routes = [
     { path: 'home', component: MainComponent },
 ];
 
-
+// update @NgModule declaration to be as follows:
 @NgModule({
     imports: [
         BrowserModule,
@@ -23,15 +25,19 @@ export const ROUTES: Routes = [
         BrowserAnimationsModule,
         RouterModule.forChild(ROUTES),
 
-
         TooltipModule.forRoot(),
     ],
     declarations: [
         MainComponent,
+        SquarePipe
     ],
 
     exports: [
         MainComponent,
     ],
+
+    providers: [
+        UserService,
+    ]
 })
 export class MainModule {}
